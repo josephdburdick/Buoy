f8e4ce719c82fa3e721f1c0cb3322b2a1e7e76b2
Yonathan Benolol <yonathan.benolol@gmail.com>
2016-01-15 14:49:35 +0200

    Moved userDetails into user. Also useMeteor.users collection now.

--- collections/users/users.js
+++ collections/users/users.js
@@ -1,11 +1,83 @@
 Meteor.users.allow({
-  insert: () => false,
-  update: () => false,
-  remove: () => false
+  insert: () => allow,
+  update: () => allow,
+  remove: () => allow
 });
 
-Meteor.users.deny({
-  insert: () => true,
-  update: () => true,
-  remove: () => true
+let Schema = {};
+
+Schema.User = new SimpleSchema({
+  "userId": {
+    type: String,
+    label: "The ID of the owner of this document."
+  },
+  username: {
+    type: String,
+    // For accounts-password, either emails or username is required, but not both. It is OK to make this
+    // optional here because the accounts-password package does its own validation.
+    // Third-party login packages may not require either. Adjust this schema as necessary for your usage.
+    optional: true
+  },
+  emails: {
+    type: Array,
+    // For accounts-password, either emails or username is required, but not both. It is OK to make this
+    // optional here because the accounts-password package does its own validation.
+    // Third-party login packages may not require either. Adjust this schema as necessary for your usage.
+    optional: true
+  },
+  "emails.$": {
+    type: Object
+  },
+  "emails.$.address": {
+    type: String,
+    regEx: SimpleSchema.RegEx.Email
+  },
+  "emails.$.verified": {
+    type: Boolean
+  },
+  "profile": {
+    type: Schema.UserProfile,
+    optional: true
+  },
+  "preferences": {
+    type: Object
+  },
+  createdAt: {
+    type: Date
+  },
+  // Make sure this services field is in your schema if you're using any of the accounts packages
+  services: {
+    type: Object,
+    optional: true,
+    blackbox: true
+  },
+  heartbeat: {
+    type: Date,
+    optional: true
+  }
 });
+
+Schema.UserProfile = {
+  "firstName": {
+    type: String,
+    optional: true
+  },
+  "lastName": {
+    type: String,
+    optional: true
+  },
+  "fullName": {
+    type: String,
+    optional: true,
+    autoValue: function() {
+      let firstName = this.siblingField("firstName"),
+      lastName = this.siblingField("lastName");
+      return firstName + ' ' + lastName;
+    }
+  },
+  "biography": {
+    type: String
+  }
+};
+
+Meteor.users.attachSchema(Schema.User);
