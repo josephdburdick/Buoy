f4f3e8e9365409fac06fd4e18adb66c7a08cd428
Joe <josephdburdick@gmail.com>
2016-01-23 23:52:07 -0500

    Update settings-development from setting environment variables on server startup. Add error message and instruction if environment variables aren't found.

--- server/modules/set-environment-variables.js
+++ server/modules/set-environment-variables.js
@@ -1,6 +1,14 @@
 let setEnvironmentVariables = () => {
   if ( Meteor.settings.private ) {
-    process.env.MAIL_URL = Meteor.settings.private.MAIL_URL;
+    Meteor.settings.private.MAIL_URL = process.env.MAIL_URL;
+		Meteor.settings.private.services.facebook.appId = process.env.FB_appId;
+		Meteor.settings.private.services.facebook.secret = process.env.FB_appSecret;
+  }
+  if ( typeof Meteor.settings.private.services.facebook.appId === "undefined" || typeof Meteor.settings.private.services.facebook.secret === "undefined") {
+    console.error('\n\n\n\n+++++++++++++++++++++++\n');
+    console.error(process.env.npm_package_name + ' v' + process.env.npm_package_version + '\n');
+    console.error('ERROR: No environmental variables set. \n\nBefore running this project \nFB_appId and FB_appSecret environment variables \nmust be applied on this machine.');
+    console.error('\n+++++++++++++++++++++++');
   }
 };
 
