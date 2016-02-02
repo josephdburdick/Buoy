d225361c5c45ea84be4d955b91771301b649e145
Joe <josephdburdick@gmail.com>
2016-02-02 08:51:07 -0500

    Add modal size option to trigger.

--- client/templates/globals/modal.js
+++ client/templates/globals/modal.js
@@ -1,12 +1,15 @@
 Template.body.events({
   'click [data-toggle="modal"]': function(event, template){
     let name = event.currentTarget.dataset.modalTemplate;
+		let modalSize = !!event.currentTarget.dataset.modalSize ? event.currentTarget.dataset.modalSize : null;
 		FlowRouter.setQueryParams({modal: true});
+		Session.set('modalSize', modalSize);
     Session.set('activeModal', name);
   },
 	'hidden.bs.modal #modal': function(event, template){
 		console.log(event);
 		FlowRouter.setQueryParams({modal: null});
+		Session.set('modalSize', null);
 	}
 });
 
@@ -16,5 +19,8 @@ Template.modal.helpers({
   },
 	modalIsVisible: function(){
 		return !!Session.get('activeModal') ? 'in' : '';
+	},
+	modalSize: function(){
+		return !!Session.get('modalSize') ? `modal-${Session.get('modalSize')}` : '';
 	}
 });
