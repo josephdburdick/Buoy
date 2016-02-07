b3f44c8ed162aa1ff98520e246f8d6868863a80f
Joe <josephdburdick@gmail.com>
2016-02-06 23:45:36 -0500

    Add event insert functionality.

--- collections/events.js
+++ collections/events.js
@@ -1,113 +1,157 @@
 Events = new Meteor.Collection('events');
 
 Events.allow({
-  insert: () => false,
-  update: () => false,
-  remove: () => false
-});
-
-Events.deny({
   insert: () => true,
   update: () => true,
   remove: () => true
 });
 
+Events.deny({
+  insert: () => false,
+  update: () => false,
+  remove: () => false
+});
+
+// SimpleSchema.messages({
+//   lonOutOfRange: '[label] longitude should be between -90 and 90',
+//   latOutOfRange: '[label] latitude should be between -180 and 180'
+// })
 EventsSchema = new SimpleSchema({
 	ownerId: {
 		type: String,
-		optional: false
+		optional: true,
+		autoform: {
+    	omit: true
+    }
 	},
   fbId: {
     type: String,
-    optional: true
+    optional: true,
+		autoform: {
+    	omit: true
+    }
   },
 	name: {
 		type: String,
 		optional: true,
-		autoValue: function(){
-			let
-				time = !!this.field('start_time').value ? this.field('start_time').value : new Date(),
-				dayPeriod = Modules.both.getDayPeriod(time),
-				humanizedDate = `${moment(time).format("dddd")} ${dayPeriod}, ${moment(time).format("MMMM Do YYYY, h:mm:ss")}`;
-				console.log(humanizedDate);
-			return humanizedDate;
+		autoform: {
+			placeholder: "Event Name"
 		}
 	},
   description: {
     type: String,
-  	optional: true
+  	optional: true,
+		autoform: {
+      afFieldInput: {
+        type: "textarea"
+      },
+			placeholder: "Event Description"
+    }
   },
   places: {
-    type: [Object],
-    optional: true,
-		label: "The place IDs for this event",
+    type: Array,
+    optional: true
   },
+	"places.$": {
+		type: Object
+	},
 	"places.$._id": {
 		type: String,
-		optional: true
+		optional: true,
+		autoform: {
+    	omit: true
+    }
 	},
 	"places.$.ownerId": {
 		type: String,
-		optional: true
+		optional: true,
+		autoform: {
+    	omit: true
+    }
 	},
 	"places.$.name": {
 		type: String,
 		optional: true,
-		defaultValue: function() {
-			return "Default Event Name";
+		autoform: {
+			placeholder: "Place Name"
 		}
 	},
 	"places.$.address": {
 		type: String,
-		optional: true
+		optional: true,
+		autoform: {
+			placeholder: "Street Address"
+		}
 	},
 	"places.$.coords": {
 		type: [Number],
-		optional: true,
-		defaultValue: [],
-		min: 0,
-		max: 2
+    decimal: true,
+    minCount: 2,
+    maxCount: 2
 	},
   itineraryId: {
     type: String,
-  	optional: true //will want to create an itinerary first and push Id or array here...
+  	optional: true, //will want to create an itinerary first and push Id or array here...
+		autoform: {
+    	omit: true
+    }
   },
   picture: {
     type: String,
-  	optional: true
+  	optional: true,
+		autoform: {
+    	omit: true
+    }
   },
   privacy: {
     type: String,
-  	optional: true
+  	optional: true,
+		autoform: {
+			placeholder: "true"
+		}
   },
   'ticket_uri': {
     type: String,
-  	optional: true
+  	optional: true,
+		autoform: {
+    	omit: true
+    }
   },
 	'start_time': {
-		type: String,
-		optional: true
+		type: Date,
+		optional: true,
+		autoValue: function() { return new Date() }
 	},
 	'end_time': {
-		type: String,
-		optional: true
+		type: Date,
+		optional: true,
+		autoValue: function() { return new Date() }
 	},
   'updated_time': {
     type: String,
-  	optional: true
+  	optional: true,
+		autoform: {
+    	omit: true
+    }
   },
   eventChannel: {
     type: Object,
-    optional: true //will want to create an channel first and push Id or array here...
+    optional: true, //will want to create an channel first and push Id or array here...
+		autoform: {
+    	omit: true
+    }
   },
 	createdAt: {
     type: Date,
 		optional: true,
+		autoform: {
+			omit: true
+		},
     autoValue: function() {
       if (this.isInsert) {
         return new Date();
       } else if (this.isUpsert) {
-        return {$setOnInsert: new Date()};
+        return { $setOnInsert: new Date() };
       } else {
         this.unset();  // Prevent user from supplying their own value
       }
@@ -121,9 +165,13 @@ EventsSchema = new SimpleSchema({
       }
     },
     denyInsert: true,
-    optional: true
+    optional: true,
+		autoform: {
+    	omit: true
+    }
   }
 });
 
 Events.attachSchema(EventsSchema);
+SimpleSchema.debug = true;
 Schema.Events = EventsSchema;
