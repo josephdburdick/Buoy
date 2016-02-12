7e68d6ae388360400deafca31b5dbabd99251d58
Joe <josephdburdick@gmail.com>
2016-02-12 08:39:16 -0500

    Rename to activeModalTemplate, use global listener in onCreated() callback for reuse.

--- client/templates/globals/modal/modal.js
+++ client/templates/globals/modal/modal.js
@@ -1,20 +1,23 @@
-Template.body.events({
-  'click [data-toggle="modal"]': function(event, template){
-    let name = event.currentTarget.dataset.modalTemplate;
-		let modalSize = !!event.currentTarget.dataset.modalSize ? event.currentTarget.dataset.modalSize : null;
-		Session.set('modalSize', modalSize);
-    Session.set('activeModal', name);
+Template.modal.onRendered(() => {
+	let self = Template.instance();
 
-		$('#modal').off('hidden.bs.modal').on('hidden.bs.modal', (event) => {
-			Session.set('modalSize', null);
-			Session.set('activeModal', null);
-		});
-  }
-});
+	$('[data-toggle="modal"]').off('click').on('click', (event) => {
+		let name = event.currentTarget.dataset.modalTemplate;
+			let modalSize = !!event.currentTarget.dataset.modalSize ? event.currentTarget.dataset.modalSize : null;
+			Session.set('modalSize', modalSize);
+	    Session.set('activeModalTemplate', name);
+
+			$('#modal').off('hidden.bs.modal').on('hidden.bs.modal', (event) => {
+				Session.set('modalSize', null);
+				Session.set('activeModalTemplate', null);
+			});
+	});
+})
+
 
 Template.modal.helpers({
-  activeModal: function(){
-    return Session.get('activeModal');
+  activeModalTemplate: function(){
+    return Session.get('activeModalTemplate');
   },
 	modalSize: function(){
 		return !!Session.get('modalSize') ? `modal-${Session.get('modalSize')}` : '';
