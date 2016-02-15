418bc65c743590db14bc075693c656edac57c8be
Joe <josephdburdick@gmail.com>
2016-02-15 08:21:52 -0500

    Add/refactor insertFacebookEvent to prevent dupes.

--- both/methods/upsert/upsertFacebookEvent.js
+++ both/methods/upsert/upsertFacebookEvent.js
@@ -1,32 +1,24 @@
-if (Meteor.isServer){
-	Meteor.methods({
-	  upsertUserFacebookEvent( event ) {
-	    if (!Meteor.userId())
-	      throw new Meteor.Error("not-authorized");
-
-	    check( event, Object );
-			console.log(event);
-			let documentId;
-			try {
-				if (!!event._id) {
-					documentId = FacebookEvents.upsert( {
-						fbId: event.fbId,
-						ownerId: Meteor.userId()
-					}, {
-						$set: event
-					});
-					return documentId.insertedId;
-				} else {
-					documentId = FacebookEvents.upsert({
-						fbId: event.fbId
-					}, {
-						$set : event
-					});
-					return documentId.insertedId;
-				}
-			} catch( exception ) {
-				return exception;
-			}
+Meteor.methods({
+  upsertUserFacebookEvent( event ) {
+    if (!Meteor.userId()){
+			throw new Meteor.Error("not-authorized");
 		}
-	});
-}
+    check( event, Object );
+		let documentId;
+		try {
+			if (!!event._id) {
+				FacebookEvents.upsert( {
+					_id: event._id
+				}, {
+					$set: event
+				});
+				return event._id;
+			} else {
+				documentId = FacebookEvents.insert(event);
+				return documentId;
+			}
+		} catch( exception ) {
+			return exception;
+		}
+	}
+});
